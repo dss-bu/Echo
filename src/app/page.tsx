00615b9c8173eee8385c91eb7c16dca2409aa2d4
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-10-29 23:15:11 +0530

    feat(test): Testing PR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
-  return <ThemeToggle />;
+  return (
+    <>
+      <ThemeToggle />
+      <h1 className="text-3xl font-bold underline">Test COMMIT , PR ok ?</h1>
+    </>
+  );
 }
